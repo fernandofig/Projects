27802d22ab33a1e5ff4d73e9aa918a4597a1677a
fernandofig <fernando.fig@gmail.com>
2013-06-13 12:44:40 -0300

    Small tweaks

--- activewriter/trunk/src/Debugging/Run.bat
+++ activewriter/trunk/src/Debugging/Run.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Microsoft Visual Studio 9.0\Common7\IDE\devenv.exe" /ranu /rootsuffix Exp /DesignTimeRun Debugging.sln
+"C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\devenv.exe" /ranu /rootsuffix Exp /DesignTimeRun Debugging.sln
